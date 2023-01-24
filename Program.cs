753866da43b9991e08cd5e9f212be80713fa6292
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-01-24 21:45:43 +0100

    Where Combining

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
 ﻿
+class Program {
+  public static void Main() {
+    var source = Enumerable.Range(1, 5);
+
+    try {
+      var acoll = source
+        .Where(x => x >= 1)
+        .Select(i => 100 - i)
+        .Where(x => x <= 100)
+        .Select(x => 2 * x)
+        .Where(x => true)
+        .Select(fail);
+      acoll.GetEnumerator().MoveNext();
+    } catch (Exception e) {
+      Console.WriteLine(e.StackTrace);
+    }
+
+    T fail<T>(T val) =>throw new Exception();
+  }
+}

c1e59f95f6926af600bc119ea8aabe51a2db565e
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-12-20 16:43:14 +0100

    frozen collections 1

--- Program.cs
+++ Program.cs
@@ -8,23 +8,29 @@ BenchmarkSwitcher.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies()).Run(ar
 
 [MemoryDiagnoser, DisassemblyDiagnoser]
 public class Bench {
-  [Params(10_000L,100_000L,1_000_000L)]
+  [Params(10_000L, 100_000L, 1_000_000L)]
   public int Count { get; set; }
   ImmutableArray<int> test = ImmutableArray<int>.Empty;
   int[]? items;
-  Dictionary<string, int>? d;
-  ImmutableDictionary<string, int>? id ;
-  FrozenDictionary<string, int>? fd ;
-  
+  Dictionary<string, int> d = new Dictionary<string, int> { };
+  ImmutableDictionary<string, int> id = ImmutableDictionary<string, int>.Empty;
+  FrozenDictionary<string, int> fd = FrozenDictionary<string, int>.Empty;
+  IEnumerable<int> set = Enumerable.Empty<int>();
+
 
   [GlobalSetup]
   public void BenchSetup() {
     items = new int[Count];
     Array.Fill(items, 0x12345678); // Range(1, Count).ToArray();
-    var set = Enumerable.Range(0, Count).Select(i => new KeyValuePair<string, int>(i.ToString(), 31*i%17)).ToArray();
-    d = new Dictionary<string, int>(set);
-    fd = d.ToFrozenDictionary();
-    id = d.ToImmutableDictionary();
+    set = Enumerable.Range(0, Count);
+    d = set.ToDictionary(i => i.ToString(), i => 31 * i % 17);
+    fd = set.ToFrozenDictionary(i => i.ToString(), i => 31 * i % 17);
+    id = set.ToImmutableDictionary(i => i.ToString(), i => 31 * i % 17);
+  }
+
+  [IterationSetup]
+  public void ItSetup() {
+    Random r = new Random(5214);
   }
 
   [Benchmark]
@@ -40,20 +46,20 @@ public class Bench {
   public void LookupImmutableDict() {
     var dict = id[Random.Shared.Next(Count).ToString()];
   }
-  
+
   [Benchmark]
-  public void  BuildDictTest() {
-    var dict = new Dictionary<string, int>(d!);
+  public void BuildDictTest() {
+    var dict = set.ToFrozenDictionary(i => i.ToString(), i => 31 * i % 17);
   }
 
   [Benchmark]
-  public void  BuildFrozenDictTest() {
-    var dict = d!.ToFrozenDictionary();
+  public void BuildFrozenDictTest() {
+    var dict = set.ToFrozenDictionary(i => i.ToString(), i => 31 * i % 17);
   }
 
   [Benchmark]
   public void BuildImmutableDictTest() {
-    var dict = d!.ToImmutableDictionary();
+    var dict = set.ToImmutableDictionary(i => i.ToString(), i => 31 * i % 17); ;
   }
 
 }
